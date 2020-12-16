34753c496c92af06f0c45f51e6d7ff8f3b8fc6e0
lee <lee@logicon.io>
2020-12-15 21:19:08 -0300

    Title: Redirects and gitignore
    Body:
      - Added redirects
      - Added sqlite3 to gitignore

--- covid/urls.py
+++ covid/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
+from django.shortcuts import redirect
 
 from . import views
 
 
 urlpatterns = [
+    path('', lambda request: redirect('index/', permanent=True)),
     path("index/", views.index, name="covid_index"),
 ]
--- data_dashboard/urls.py
+++ data_dashboard/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
+from django.shortcuts import redirect
 
 
 urlpatterns = [
+    path('', lambda request: redirect('covid/index', permanent=True)),
     path('admin/', admin.site.urls),
     path('covid/', include('covid.urls')),
 ]
